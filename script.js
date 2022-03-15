43760043f919ad24c876afd9e5e7c96bbb125882
Yuliya <yulya.kremko@mail.ru>
2022-03-15 14:44:35 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+ "use strict";
+ 
+const category = 'toys';
+console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Юлия';
+alert(`Привет, ${user}`);
+
+
+console.log(4 + 5);
+
+let incr = 10,
+    decr = 10;
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(26%9);
+
+console.log(2*4 === 8);
+
+const isChecked = false,
+      isClose = false;
+console.log(isChecked || !isClose);
