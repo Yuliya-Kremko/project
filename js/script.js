92e70c27ab582e5a4f2700aec83ae567089b6b69
Yuliya <yulya.kremko@mail.ru>
2022-04-01 18:26:45 +0400

    Homework

--- js/script.js
+++ js/script.js
@@ -12,15 +12,32 @@
 
 start();
 
- const personalMoviDB = {
+ let personalMoviDB = {
      count: NumberOfFilms,
      movies: {},
      actors: {},
      genres: [],
-     privat: false
+     privat: false,
+     showMyDB: function (hidden) {
+        if (!hidden) {
+            console.log(personalMoviDB);
+        }
+      }
+    
  };
 
- function detectPersonalLevel() {
+ personalMoviDB = {
+   toggleVisibleMyDB: function () {
+    if (personalMoviDB.privat == true) {
+        personalMoviDB.privat = false;
+    } else {
+        personalMoviDB.privat = true;
+    }
+  }
+};  
+
+ personalMoviDB = {
+    detectPersonalLevel() {
     if (personalMoviDB.count < 10) {
         alert (`Просмотрено довольно мало фильмов`);
     } else if (personalMoviDB.count >=10 && personalMoviDB.count< 30) {
@@ -30,11 +47,12 @@ start();
     } else  {
         alert (`Произошла ошибка`);
     }
-    
-}
-detectPersonalLevel();
 
-function rememberMyFilms() {
+  }
+};
+
+  personalMoviDB = {
+  rememberMyFilms: function (detectPersonalLevel, callback) {
     for (let i=0; i<2; i++ ) {
          const NameFilms= prompt (`Один из последних просмотренных фильмов?` , ``),
                GradeFilms = +prompt (`На сколько оцените его?` , ``);
@@ -44,23 +62,13 @@ function rememberMyFilms() {
             i--;
         }
     }
-}
-rememberMyFilms();
-
-
-function writeYourGenres () {
+  },
+  writeYourGenres: function () {
     for (let i=0; i<3;i++) {
          personalMoviDB.genres [i] = prompt (`Ваш любимый жанр под номером ${i+1}` , ``);
          if (personalMoviDB.genres == ``) {
              i--;
          }
     }
-}
-writeYourGenres();
-
-function showMyDB (hidden) {
-    if (!hidden) {
-        console.log(personalMoviDB);
-    }
-}
-showMyDB(personalMoviDB.privat);
+  }
+};
