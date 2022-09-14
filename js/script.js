3ff7c60d38a5f1ea82b650d4cf8c91bbf4e9cf6d
Yuliya <yulya.kremko@mail.ru>
2022-09-14 21:48:20 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+// const result = confirm ( `Are you here? ` );
+// console.log(result);
+
+// const answer = prompt (` Вам есть 18 лет? ` , `18`);
+// console.log( answer);
+
+const answers = [];
+
+// answers[0] =  prompt (`Как Вас зовут?` , ``);
+// answers[1] =  prompt (`Какая у Вас фамилия?` , ``);
+// answers[2] =  prompt (`Сколько Вам лет?` , ``);
+
+// document.write(answers);
+// console.log( typeof(answers));
+
+let user;
+user = prompt (`Как Вас зовут?`, ``);
+alert (`Привет, ${user}`);
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(incr);
+console.log(decr);
